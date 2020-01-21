0baf36d7637c76bb7b04f6e36738a32a3977c8fa
Simion Vlad <simivlad23@yahoo.ro>
2020-01-21 13:56:34 +0200

    code refactor

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TabHost;
 
 import java.util.ArrayList;
 
@@ -28,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<String> mDeviceList = new ArrayList<>();
     BluetoothGatt bluetoothGatt;
     private static final String TAG = "BLE-app";
+    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    ArrayAdapter<String> listAdapter;
 
     private final double RF_A =40; // the absolute energy which is represent by dBm at a distance of 1 meter from the transmitter
     private final double RF_N =2.3; // n is the signal transmission constant
@@ -38,20 +41,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView = findViewById(R.id.listView);
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
         final Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-        final ArrayAdapter<String> listAdapter = new ArrayAdapter<>(getApplication().getBaseContext(),
+        listAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, mDeviceList);
 
+        listView.setAdapter(listAdapter);
 
-        listView.setAdapter(new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, mDeviceList));
         Button buuton = findViewById(R.id.button1);
+
         buuton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                listView.setAdapter(null);
-                for(BluetoothDevice bt : pairedDevices) {
 
+                for(BluetoothDevice bt : pairedDevices) {
                     bluetoothGatt =  bt.connectGatt(MainActivity.this, true, gattCallback);
                     listView.setAdapter(listAdapter);
 
@@ -80,12 +82,13 @@ public class MainActivity extends AppCompatActivity {
         public void onReadRemoteRssi(BluetoothGatt gatt, int rssi, int status) {
             super.onReadRemoteRssi(gatt, rssi, status);
 
+            Log.d(TAG,"status is "+ status);
            if(status == BluetoothGatt.GATT_SUCCESS){
 
                //TODO save the rssi values in a file
-
-                mDeviceList.add(gatt.getDevice().getName() + "\n" + gatt.getDevice().getAddress() + " RSSI: " + rssi);
-                Log.d(TAG,String.format("BluetoothGatt ReadRssi [%d]",rssi));
+                //mDeviceList.add(gatt.getDevice().getName() + "\n" + gatt.getDevice().getAddress() + " RSSI: " + rssi);
+                //listAdapter.notifyDataSetChanged();
+                Log.d(TAG,String.format("BluetoothGatt ReadRssi from "+ gatt.getDevice().getAddress()+ " value : [%d]  and distance calculated :" + getDistance(rssi,1),rssi));
                 Log.i(TAG,"Distance is: "+ getDistance(rssi,1));
             }
         }
